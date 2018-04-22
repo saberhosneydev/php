59ea162fc99dd34c7bd2c0bc504f26fcbb5365db
Saber Hosney <saber20_13@hotmail.com>
2018-04-22 23:43:37 +0200

    Add files via upload

---
+++ fileHosting/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE>
+<html>
+<head>
+<title>Online file storage</title>
+</head>
+<body>
+    <form action="upload.php" method="POST" enctype="multipart/form-data">
+        <input type="file" name="file">
+        <button type="submit" name="submit">Upload</button>
+    </form>
+</body>
+</html>
---
+++ fileHosting/upload.php
@@ -0,0 +1,39 @@
+<?php
+
+if (isset($_POST['submit'])) {
+    $file = $_FILES['file'];
+    $fileName = $_FILES['file']['name'];
+    $fileTmpName = $_FILES['file']['tmp_name'];
+    $fileSize = $_FILES['file']['size'];
+    $fileError = $_FILES['file']['error'];
+    
+    $fileExt = explode('.', $fileName);
+    $fileActualExt = strtolower(end($fileExt));
+    
+//    $allowed = array('jpg', 'jpeg', 'mp4', 'png');
+//    
+//    if (in_array($fileActualExt, $allowed)){
+//        if ($fileError === 0) {
+//            $fileNameNew = uniqid('', true).".".$fileActualExt;
+//            $fileDest = 'uploads/'.$fileNameNew;
+//            move_uploaded_file($fileTmpName, $fileDest);
+//            header("Location: index.php?success=1");
+//        }else {
+//            echo "error in fileErrror";
+//        }
+//    }else {
+//        echo "Error in EXT";
+//    }
+// ABOVE code is used to upload a filtered extensions ^^
+    
+        if ($fileError === 0) {
+            $fileNameNew = uniqid('', true).".".$fileActualExt;
+            $fileDest = 'uploads/'.$fileNameNew;
+            move_uploaded_file($fileTmpName, $fileDest);
+            header("Location: index.php?success=1");
+        }else {
+            echo "error in fileErrror";
+        }
+}
+
+?>
