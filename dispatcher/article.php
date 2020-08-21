16856611828783522c15a7bd48e1a378d2770d0a
saberhosneydev <ssber277@gmail.com>
2020-08-22 00:34:36 +0200

    base structure done - building the dashboard

--- dispatcher/article.php
+++ dispatcher/article.php
@@ -1,14 +1,11 @@
 <?php 
-$filename = "";
+$json = "";
  if(isset($_GET['date']) && isset($_GET['slug'])){
     $filename = $_GET['date']."_".$_GET['slug'].".json";
     $temp = scandir("articles");
     if(array_search($filename, $temp)){
         $data = file_get_contents("articles/$filename");
-        $data = json_decode($data, true);
-        foreach($data as $key => $value){
-            echo $value;
-        }
+        $json = json_decode($data, true);
     }else {
         include("pages/404.html");
     }
@@ -16,3 +13,13 @@ $filename = "";
      include("pages/404.html");
  }
 ?>
+<div class="blog-post w-8/12 m-auto mt-5">
+    <img src="/assets/images/<?php echo $json['image'] ?>" style="height:400px;width:100%;" alt="">
+    <div class="text-center mt-2 mb-2">
+    <h1 class="text-2xl text-purple-800"><?php echo $json['title'] ?></h1>
+    <p class="text-md text-blue-900">created at: <span><?php echo $json['date'] ?></span> - Category: <span><?php echo $json['category'] ?></span> - by: <span><?php echo $json['author'] ?></span></p>
+    </div>
+    <p class="text-gray-700 p-2 pb-5">
+    <?php echo str_replace("\n", "<br>", $json['content']) ?>
+    </p>
+</div>
--- dispatcher/index.php
+++ dispatcher/index.php
@@ -10,6 +10,7 @@
 <body>
     <?php include("layouts/header.html");?>
     <?php 
+        $data = false;
         if(isset($_GET['page'])){
             switch ($_GET['page']) {
                 case 'about':
@@ -23,9 +24,29 @@
                     break;
             }
         }else {
-            include("layouts/body.html");
+            $data = scandir("articles");
+        }
+        function getFile($filename){
+            $file = file_get_contents("articles/$filename");
+            return json_decode($file, true);
         }
     ?>
+    <?php if($data): ?>
+        <div class="w-8/12 m-auto text-center lg:text-left">
+        <?php for($i=2; $i < count($data); $i++): ?>
+    <div class="article mt-5 mb-5 grid gap-4 grid-cols-3 lg:grid-cols-4">
+        <img src="../assets/images/<?php echo getFile($data[$i])['image']; ?>" alt="" class="hover:opacity-75 rounded col-span-1 hidden lg:block">
+        <div class="input-form  col-span-4 lg:col-span-3">
+            <a href="/<?php echo getFile($data[$i])['slug']; ?>.html" class="text-xl text-blue-600"><?php echo getFile($data[$i])['title']; ?></a>
+            <p class="text-sm text-blue-900">created at: <span><?php echo getFile($data[$i])['date']; ?></span> - Category: <span><?php echo getFile($data[$i])['category']; ?></span></p>
+            <p class="text-md truncate"><?php echo getFile($data[$i])['content']; ?></p>
+        </div>
+    </div>
+        <?php endfor; ?>
+    </div>
+        
+    <?php endif; ?>
+
 <?php include("layouts/footer.html");?>
 </body>
 </html>
